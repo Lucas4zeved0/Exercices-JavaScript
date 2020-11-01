6a212651d3e2a04083422c2730f2e39e50e9063f
Lucas Azevedo <71043054+Lucas4zeved0@users.noreply.github.com>
2020-11-01 16:38:37 -0300

    Update exercicio01.js

--- Exercices-Of-JavaScript/exercicio01.js
+++ Exercices-Of-JavaScript/exercicio01.js
@@ -1,19 +1,19 @@
-function calculosAritmeticos(valor1, valor2) {
+function AritmeticCalculations(value1, value2) {
 
-    const soma = valor1 + valor2
-    const subtracao = valor1 - valor2
-    const multiplicacao = valor1 * valor2
-    const divisao = valor1 / valor2
+    const sum = value1 + value2
+    const subtraction = value1 - value2
+    const multiplication = value1 * value2
+    const division = value1 / value2
 
-    console.log(`A soma entre os dois valores é = ${soma.toFixed(2)}`)
-    console.log(`A subtração entre os dois valores é = ${subtracao.toFixed(2)}`)
-    console.log(`A multiplicação entre os dois valores é = ${multiplicacao.toFixed(2)}`)
-    console.log(`A divisão entre os dois valores é = ${divisao.toFixed(2)}`)
+    console.log(`The sum between the two values is = ${sum.toFixed(2)}`)
+    console.log(`The subtraction between the two values is = ${subtraction.toFixed(2)}`)
+    console.log(`The multiplication between the two values is = ${multiplication.toFixed(2)}`)
+    console.log(`The division between the two values is = ${division.toFixed(2)}`)
 
     console.log('================================================')
 }
 
-calculosAritmeticos(1, 1)
-calculosAritmeticos(1, 3)
-calculosAritmeticos(3.23, 5.96)
-calculosAritmeticos(2.8371491791, 9.3749187349817)
+AritmeticCalculations(1, 1)
+AritmeticCalculations(1, 3)
+AritmeticCalculations(3.23, 5.96)
+AritmeticCalculations(2.8371491791, 9.3749187349817)
