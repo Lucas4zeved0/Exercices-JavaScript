125cea6c439f72ed24dfd79481ce74ef54e90132
Lucas Azevedo <71043054+Lucas4zeved0@users.noreply.github.com>
2020-11-02 18:35:39 -0300

    Update exercice04.js

--- Exercices-Of-JavaScript/exercice04.js
+++ Exercices-Of-JavaScript/exercice04.js
@@ -1,25 +1,24 @@
-//9/4 9 divisor e 4 dividendo 
-
-function calculoDivEResto(divisor, dividendo) {
-    const resultadoDaDivisao = divisor / dividendo
-    const restoDaDivisao = divisor % dividendo
+//
+function calculationDivAndRest(divider, dividend) {
+    const resultOfDivision = divider / dividend
+    const restOfDivision = divider % dividend
     
-    // Resultado da divisão
-    if(Number.isInteger(resultadoDaDivisao) == true) {
-        console.log(`O resultado da divisão entre ${divisor} e ${dividendo} é = ${resultadoDaDivisao.toFixed(0)}`)
+    // Result of division
+    if(Number.isInteger(resultOfDivision) == true) {
+        console.log(`The result of division between ${divider} and ${dividend} is = ${resultOfDivision.toFixed(0)}`)
     } else {
-        console.log(`O resultado da divisão entre ${divisor} e ${dividendo} é = ${resultadoDaDivisao.toFixed(2)}`)
+        console.log(`The result of division between ${divider} and ${dividend} is = ${resultOfDivision.toFixed(2)}`)
     }
 
-    //Resto da divisão
-    if(Number.isInteger(restoDaDivisao) == true) {
-        console.log(`O resto da divisão entre ${divisor} e ${dividendo} é = ${restoDaDivisao.toFixed(0)}`)
+    //Rest of division
+    if(Number.isInteger(restOfDivision) == true) {
+        console.log(`The rest of division between ${divider} and ${dividend} is = ${restOfDivision.toFixed(0)}`)
     } else {
-        console.log(`O resto da divisão entre ${divisor} e ${dividendo} é = ${restoDaDivisao.toFixed(2)}`)
+        console.log(`The rest of division between ${divider} and ${dividend} is = ${restOfDivision.toFixed(2)}`)
     }
     console.log('============================================================')
 } 
 
-calculoDivEResto(2, 2)
-calculoDivEResto(5, 3)
-calculoDivEResto(28, 3.14)
+calculationDivAndRest(2, 2)
+calculationDivAndRest(5, 3)
+calculationDivAndRest(28, 3.14)
