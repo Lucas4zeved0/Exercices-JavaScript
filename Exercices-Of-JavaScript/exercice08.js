63b3c3a5f3372dd9eabd1d203e8145b214f53b25
Lucas Azevedo <71043054+Lucas4zeved0@users.noreply.github.com>
2020-11-03 19:14:55 -0300

    Create exercice08.js

---
+++ Exercices-Of-JavaScript/exercice08.js
@@ -0,0 +1,15 @@
+function avaliaPontuacoes(pontuacoes) {
+    let recebePontuacoes = pontuacoes.split(" ")
+    let bateuRecord = 0
+    let maiorPontuacao = 0
+    let menorPontuacao = 1
+
+    for(i = 1; i < recebePontuacoes.length; i++){
+
+        if()
+
+    }
+
+}
+
+avaliaPontuacoes("10 20 30 20 40 50 0 1 4")
