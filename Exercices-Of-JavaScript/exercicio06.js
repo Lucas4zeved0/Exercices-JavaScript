6bed4fcea27468cec0ff52146710138bce148819
Lucas Azevedo <71043054+Lucas4zeved0@users.noreply.github.com>
2020-11-03 18:57:22 -0300

    Update exercicio06.js

--- Exercices-Of-JavaScript/exercicio06.js
+++ Exercices-Of-JavaScript/exercicio06.js
@@ -1,25 +1,17 @@
-/*
-* M = C(1 + i) ** n
-* M = montante
-* C = Capital inicial
-* i = taxa de juros
-* n = número de períodos que o capital ficou aplicado
-*/
-
-function montanteComJurosSimples(capitalInicial, taxaJuros, tempoAplicacao) {
-    let calculoDoMontante = capitalInicial * (1 + (taxaJuros/100) * tempoAplicacao) 
-    console.log('O valor do montante com juros simples é ' + calculoDoMontante.toFixed(2))
+function amountSimpleInterest(initialCapital, rateInterest, applicationTime) {
+    let amountCalculation = initialCapital * (1 + (rateInterest/100) * applicationTime) 
+    console.log('The value of amount with simple interest is ' + amountCalculation.toFixed(2))
 }
 
-function montanteComJurosCompostos(capitalInicial, taxaJuros, tempoAplicacao) {
-    let calculoDoMontante = capitalInicial * (1 + (taxaJuros/100)) ** tempoAplicacao
-    console.log('O valor do montante com juros compostos é ' + calculoDoMontante.toFixed(2))
+function amountCompoundInterest(initialCapital, rateInterest, applicationTime) {
+    let amountCalculation = initialCapital * (1 + (rateInterest/100)) ** applicationTime
+    console.log('The value of amount with compound interest is ' + amountCalculation.toFixed(2))
 }
 
 
-montanteComJurosSimples(10, 10, 5)
-montanteComJurosSimples(240, 19.88, 12)
-montanteComJurosSimples(100, 10, 2)
-montanteComJurosCompostos(10, 10, 3) 
-montanteComJurosCompostos(283.33, 23.12, 5)
-montanteComJurosCompostos(100, 10, 2)
+amountSimpleInterest(10, 10, 5)
+amountSimpleInterest(240, 19.88, 12)
+amountSimpleInterest(100, 10, 2)
+amountCompoundInterest(10, 10, 3) 
+amountCompoundInterest(283.33, 23.12, 5)
+amountCompoundInterest(100, 10, 2)
